0bcca4d81a83369580a173b09a4d6deb5c02aa0a
alexweav <warriordude417@suddenlink.net>
2016-09-27 11:16:39 -0500

    Modified zero-padding to allow for tensors containing multiple images

--- LearnyMcLearnface/Utils/Image.py
+++ LearnyMcLearnface/Utils/Image.py
@@ -8,15 +8,14 @@ Created on Tue Sep 27 10:26:55 2016
 import numpy as np
 
 """
-Takes a 2 or 3 dimensional numpy array
-Assumes that dimensions 1 and 2 represent x and y pixel coordinates in the image, respectively
-Assumes that if a third dimension is present, it represents channels of the image (rgb, etc.)
-Pads the image with the indicated amount of zeroes along the first and second (spatial) dimensions only
+Takes a 4 dimensional image tensor.
+The first coordinate indexes images in the tensor.
+The second coordinate indexes channels for each image.
+The third coordinate indexes the y-coordinate of pixels in each channel.
+The fourth coordinate indexes the x-coordinate of pixels in each channel.
+We only want to pad the images across their spatial dimensions, so we only pad across the last two dimensions.
 """
 def zero_pad(X, pad):
     num_dims = len(X.shape)
-    assert num_dims == 2 or num_dims == 3, 'The objected provided for zero-padding does not look like an image.'
-    if num_dims == 2:
-        return np.pad(X, pad, 'constant')
-    if num_dims == 3:
-        return np.pad(X, ((pad, pad), (pad, pad), (0, 0)), 'constant')
+    assert num_dims == 4, 'The objected provided for zero-padding does not look like an image tensor.'
+    np.pad(X, ((0, 0), (0, 0), (pad, pad), (pad, pad)), mode='constant')
